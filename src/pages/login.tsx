3647ed394b7720223490b0691fcff3f49c9603c6
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-03-09 17:14:50 +0900

    email password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,43 +1,93 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Box, FormControl } from "@mui/material";
-import Input from '@mui/joy/Input';
+import { Box, FormControl, Grid, Input, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import LockIcon from "@mui/icons-material/Lock";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { transform } from "typescript";
+import useColorTheme from "@/hooks/UseColorTheme";
 
 type Inputs = {
-  example: string,
-  exampleRequired: string,
+  email: string;
+  password: string;
 };
 
 const login = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const { palette } = useColorTheme();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  console.log(watch("example"));
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("email"));
 
   return (
-    <Box sx={{
-      width: '100%',
-      height: "90vh",
-      position: "relative",
-
-    }}>
-      <Box sx={{
-        top: "50%",
-        left: "50%",
-        position: "absolute",
-        transform: "translate(-50%,-50%)"
-      }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "90vh",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          top: "50%",
+          left: "50%",
+          position: "absolute",
+          transform: "translate(-50%,-50%)",
+        }}
+      >
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Box sx={{ width: 200, display: "flex", flexDirection: "column" }}>
+          <Box
+            sx={{
+              width: 350,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
             <FormControl>
-              <Input defaultValue="test" {...register("example")} />
-              <Input {...register("exampleRequired", { required: true })} />
-              {errors.exampleRequired && <span>This field is required</span>}
-              <Input type="submit" />
+              <Grid
+                container
+                width="100%"
+                spacing={2}
+                direction="column"
+                alignItems="center"
+              >
+                <Grid container direction="column" alignItems="center">
+                  <Grid item>
+                    <LockIcon />
+                  </Grid>
+                  <Grid item>Sign In</Grid>
+                </Grid>
+                <Grid item width="100%">
+                  <TextField
+                    fullWidth
+                    id="outlined-controlled"
+                    label="email"
+                    {...register("email", { required: true })}
+                  />
+                  {errors.email && <span>This field is required</span>}
+                </Grid>
+                <Grid item width="100%">
+                  <TextField
+                    fullWidth
+                    id="outlined-controlled"
+                    label="password"
+                    {...register("password", { required: true })}
+                  />
+                  {errors.password && <span>This field is required</span>}
+                </Grid>
+                <Grid item sx={{ width: "100%" }}>
+                  <Button type="submit" variant="contained" fullWidth>
+                    送信
+                  </Button>
+                </Grid>
+              </Grid>
             </FormControl>
           </Box>
-        </form >
+        </form>
       </Box>
     </Box>
   );
