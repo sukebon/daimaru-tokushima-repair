5bf95383877f25d672a26ea646ff708dfaa9fe70
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-03-29 18:02:22 +0900

    npm i

--- firebase/sever.js
+++ firebase/sever.js
@@ -1,13 +1,13 @@
-const admin = require('firebase-admin');
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+// const admin = require('firebase-admin');
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+// const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
 
-if (admin.apps.length === 0) {
-  initializeApp({
-    credential: cert(JSON.parse(serviceAccount))
-  });
-}
+// if (admin.apps.length === 0) {
+//   initializeApp({
+//     credential: cert(JSON.parse(serviceAccount))
+//   });
+// }
 
-export const db = getFirestore();
+// export const db = getFirestore();
